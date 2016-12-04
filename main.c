8ad296aa199a5090254828e1bb4a7ee5766a1497
vipkry <lolpsxvipkry2@gmail.com>
2016-12-04 15:06:12 -0200

    insere atualizado

--- main.c
+++ main.c
@@ -104,18 +104,22 @@ int create(FILE* sql) {
 int insert(FILE* sql) {
     char ntable[31], ntablef[31], ndata[31], tupla[501];
     fscanf(sql, "%s", ntable); //pega o INTO
-    fscanf(sql, "%s", ntable); //colocar "INTO %s" não funcionou
+    ntable = fscanf(sql, "%s", ntable); //colocar "INTO %s" não funcionou
+
     strcpy(ntablef, ntable);
     strcpy(ndata, ntable);
     strcat(ntablef, ".ctl");
     strcat(ndata, ".dad");
+
     fscanf(sql, "%s", tupla); //pega o VALUES
     fscanf(sql, "%s", tupla); //pega a tupla, "(%s);" não funciona
+
     FILE* table = fopen(ntablef, "rt");
     if (!table) {
         printf("Tentativa de inserir em tabela nao existente: %s", ntable);
         return 1;
     }
+    fclose(table);
     FILE* data = fopen(ndata, "at+");
     if (!data) {
         printf("Erro ao criar o arquivo de dados %s", ndata);
@@ -125,15 +129,22 @@ int insert(FILE* sql) {
 
     int i = 0;
     while (tupla[i] != '\0') {
+        // tira os "(" e ");" depois traz todo o resto da string pra frente
+        if (tupla[i] == '(' || tupla[i] == ')' || tupla[i]== ';'){
+            int j = i;
+            while (tupla[j] != '\0'){
+                tupla[j] = tupla[j+1];
+                j++;
+            }
+            tupla[j] = NULL;
+        }
+
         if (tupla[i] == '"')
             tupla[i] = '\'';
         ++i;
     }
-
-    //TODO: retirar ( e ); da tupla
     //TODO: verificar chave, implementar chave ordenada, verificar nulo
     fprintf(data, "%s\n", tupla);
-    fclose(table);
     fclose(data);
     return 0;
 }
@@ -150,7 +161,7 @@ int select(FILE* sql) {
 
     char table[31], data[31];
     // "SELECT * FROM TABLE_NAME"
-    table = fscanf(sql, " FROM %s ", table);
+    table = fscanf(sql, " FROM %s ", table); // talvez esse regex não funcione
     strcpy(data, table);
     strcat(table, ".ctl");
     strcat(data, ".dad");
@@ -159,6 +170,9 @@ int select(FILE* sql) {
 
     FILE* arq_ctl = fopen(fname, "rt");
     FILE* arq_dad = fopen(fname, "rt");
+
+    fclose(arq_dad);
+    fclose(arq_ctl);
 
 
     return 0;
